d280f87ed7bf81392c47ad7a634ddf84361cfce6
perseverance51 <perseverance51@126.com>
2022-06-19 14:10:40 +0800

    first commit

--- ESP_EEPROM_Simple/ESP_EEPROM_Simple.ino
+++ ESP_EEPROM_Simple/ESP_EEPROM_Simple.ino
@@ -1,26 +1,22 @@
 // ESP8266的ESP_EEPROM库使用示例
-//
+//点击这里会自动打开管理库页面: http://librarymanager/All#ESP_EEPROM
 //通常在ESP8266上写入'仿真' EEPROM需要擦除用于保存的闪存页
 //对EEPROM数据进行重写。
 //删除需要大量的时间(10s of ms)，在此期间
 //中断必须被阻塞。
-//在某些情况下，这会干扰草图操作(例如，它会产生一个明显的
-// ESP8266 PWM依赖于中断时，任何PWM控制的灯都会停电/闪光)
-//
 // ESP_EEPROM库将每个新版本的EEPROM数据写入一个新区域，直到flash结束
 //已满，因此在必要时避免擦除flash
-//
-//当EEPROM中只有几个东西需要保存时，它是最好的
+//EEPROM尽量存取重要和少量的参数信息
 //(即保存的信息的总大小比可用的flash要小得多)
 
-struct MyObject {
+struct MyObject {//用于写入eeprom中的结构体数据
   char name[15];//15
   float field1;//4
   int field2;//2
   byte Height; //1
 };
 
-struct MyObject2 {
+struct MyObject2 {//用于读取结构体中的数据
   char name2[15];//15
   float field1a;//4
   int field2b;//2
@@ -36,17 +32,9 @@ void setup() {
   // This odd speed will show ESP8266 boot diagnostics too
   Serial.begin(9600);
   Serial.println();
-
-  // All the library functions are accessed via the EEPROM object created when
-  // you include the library header ESP_EEPROM.h
-
   // 库需要知道申请的EEPROM变量空间需要多大
-  // 最小大小为16
-
-  // The begin() call is required to initialise the EEPROM library
   EEPROM.begin(1024);
-
-  // put some data into eeprom
+  // 存放数据
   EEPROM.put(0, eepromVar1);  // int - so 4 bytes (next address is '4')
   EEPROM.put(4, eepromVar2);  // long - so 8 bytes (next address would be '12')
 MyObject customVar = {
@@ -59,14 +47,14 @@ MyObject customVar = {
   boolean ok1 = EEPROM.commit();
   Serial.println((ok1) ? "First commit OK" : "Commit failed");
 
-  // The eeprom data gets changed
-  eepromVar1 = 1;   // Change some data
+  // 存放改变后的数据
+  eepromVar1 = 9527;   // Change some data
 
-  // set the EEPROM data ready for writing
+  // 存放地址和存储对象
   EEPROM.put(0, eepromVar1);
 
   // commit (write) the data to EEPROM - only actually writes if there has been a change
-  boolean ok2 = EEPROM.commit();
+  bool ok2 = EEPROM.commit();//存储返回值，成功返回true，否则返回false
   Serial.println((ok2) ? "Second commit OK" : "Commit failed");
 
   // How to read stuff back into variables
